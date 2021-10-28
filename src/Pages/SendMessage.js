3c57e487a3ad208770bf0f678977bf01e97ac0f3
Deniz <karadagde@gmail.com>
2021-10-29 00:28:38 +0200

    added style to chat bubbles and calculating distance between users

--- src/Pages/SendMessage.js
+++ src/Pages/SendMessage.js
@@ -7,7 +7,7 @@ import {
   getDocs,
   doc,
   setDoc,
-  // getDoc,
+  //getDoc,
 } from "firebase/firestore";
 import db from "../firebase";
 import MapContainer from "../Components/Maps/MapContainer";
@@ -30,9 +30,6 @@ export default function SendMessage() {
     setInputLicensePlate(value);
   };
 
-  let currentD = distance(latitude, 52.38498, longitude, 4.813544);
-  console.log(currentD * 1000, "this is D");
-
   useEffect(() => {
     (async () => {
       navigator.geolocation.getCurrentPosition(function (position) {
@@ -52,35 +49,82 @@ export default function SendMessage() {
     try {
       const querySnapshot = await getDocs(q);
       let found_users = null;
+      let currentD;
       // eslint-disable-next-line no-unused-vars
       const receiverProfile = querySnapshot.forEach(async (userDoc) => {
         found_users = userDoc.data();
 
         const receiverID = userDoc.data().uid;
+        console.log(found_users, "reveal yourself");
         // eslint-disable-next-line no-unused-vars
         const receiverLicensePlate = await userDoc.data().licensePlate;
         const convID = user.uid + receiverID;
         const timeStamp = new Date();
-        // eslint-disable-next-line no-unused-vars
-        const newMessage = await setDoc(doc(db, "messages", convID), {
-          messageID: convID,
-          request:
-            "Hey Mate,I would like to use the charging spot at .... What time will you be leaving this spot?",
-          senderUID: user.uid,
-          senderLicensePlate: user.licensePlate,
-          response: "",
-          receiverUID: receiverID,
-          receiverLicensePlate: userDoc.data().licensePlate,
-          requestDateTime: timeStamp.toLocaleString(),
-        });
+
+        if (userDoc.data().status !== "") {
+          currentD =
+            distance(
+              latitude,
+              userDoc.data().location[0],
+              longitude,
+              userDoc.data().location[1]
+            ) * 1000;
+          console.log(currentD, "this is D");
+          if (currentD > 100) {
+            console.log("distance is too much I think", currentD);
+            return setLpError(
+              `We could not verify that you are at the same location with ${receiverLicensePlate}. Please make it sure that you are close to the car that you want to contact.`
+            );
+          } else {
+            await setDoc(doc(db, "messages", convID), {
+              messageID: convID,
+              request:
+                "Hi,I would like to use the charging spot that you are using now. What time will you be leaving this spot?",
+              senderUID: user.uid,
+              senderLicensePlate: user.licensePlate,
+              response: {
+                date: found_users.status.date,
+                time: found_users.status.time,
+              },
+              receiverUID: receiverID,
+              receiverLicensePlate: userDoc.data().licensePlate,
+              requestDateTime: timeStamp.toLocaleString(),
+            });
+            console.log("distance is small");
+          }
+        } else {
+          // eslint-disable-next-line no-unused-vars
+          const newMessage = await setDoc(doc(db, "messages", convID), {
+            messageID: convID,
+            request:
+              "Hi,I would like to use the charging spot that you are using now. What time will you be leaving this spot?",
+            senderUID: user.uid,
+            senderLicensePlate: user.licensePlate,
+            response: "",
+            receiverUID: receiverID,
+            receiverLicensePlate: userDoc.data().licensePlate,
+            requestDateTime: timeStamp.toLocaleString(),
+          });
+          console.log("status was empty");
+        }
       });
+      console.log(currentD, "is distance getting updated?");
       if (found_users === null) {
         setLpError(
           "Unfortunately this EV owner does not use our app. Please be sure that you wrote the correct lisence plate"
         );
-        setTimeout(() => setLpError(false), 3000);
-        setTimeout(() => setInputLicensePlate(""), 3000);
+        setTimeout(() => setLpError(false), 5000);
+        setTimeout(() => setInputLicensePlate(""), 5000);
+      } else if (currentD > 100 && longitude) {
+        console.log("what might be the reason you do not work?");
+        setLpError(
+          `We could not verify that you are at the same location with ${inputLicensePlate}. Please make it sure that you are close to the car that you want to contact.`
+        );
+        setTimeout(() => setLpError(false), 5000);
       } else {
+        console.log(found_users, "reveal yourself");
+        console.log(currentD, "distance at tthe bottom?");
+
         history.push("/");
       }
     } catch (e) {
@@ -106,8 +150,8 @@ export default function SendMessage() {
         <br></br>
         <MapContainer />
         <p>
-          Hey green energy user,I would like to use this chargin spot. What time
-          will you be leaving this spot?
+          <span>Your Message:</span> Hey green energy user,I would like to use
+          this charging spot. What time will you be leaving this spot?
         </p>
         <button id="send_button" onClick={() => SendMessage()}>
           Send
